6911d4e378339c723893aa139fbb263e16a242fc
afrin823 <afrin12490@gmail.com>
2024-04-21 22:17:52 +0600

    first commit

---
+++ src/Components/Root/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>Home</h2>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
